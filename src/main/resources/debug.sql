8fb1d2951da930b0ec1e9b0360d92627f1ad0179
xlc <2329962155@qq.com>
2018-04-04 22:48:04 +0800

    1 列非空测试 ; 完成非空部分,改列数20,行数100w

--- src/main/resources/debug.sql
+++ src/main/resources/debug.sql
@@ -14,4 +14,9 @@ SHOW VARIABLES LIKE '%type%';
 SHOW SCHEMAS;
 SELECT * FROM notnullcolumnmapper;
 # 通过 order by 加速 索引列列名加速
+SELECT count(id) FROM notnullcolumnmapper;
 SELECT count(id) FROM notnullcolumnmapper ORDER BY id;
+SELECT count(1) FROM notnullcolumnmapper ORDER BY id;
+
+TRUNCATE notnullcolumnmapper;
+DROP TABLE notnullcolumnmapper;
